7eb125abbc8e04dab26f1bdc4eaa97bd54834999
Loet26 <224544@lekenlinge.nl>
2026-02-04 13:24:53 +0100

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,4 @@
+import play 
+menu = play.new_box(color= "white", border_radius= 10, border_color="black", width = 200, height= 100)
+tekst = play.new_text("Welkom in de arcade")
+play.start_program
