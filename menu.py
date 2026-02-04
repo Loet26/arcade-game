620f5bc9f7182179a018fcaed64ba4f3892a0b71
Loet26 <224544@lekenlinge.nl>
2026-02-04 13:41:07 +0100

    toevoeging code

--- menu.py
+++ menu.py
@@ -1,4 +1,13 @@
 import play 
 menu = play.new_box(color= "white", border_radius= 10, border_color="black", width = 200, height= 100)
 tekst = play.new_text("Welkom in de arcade")
-play.start_program
+
+spel_1 = play.new_box (color= "black", width = 100, height= 100, x=-200, y=100)
+spel_2 = play.new_box (color= "blue", width = 100, height= 100, x=200, y=100)
+spel_3 = play.new_box (color= "green", width = 100, height= 100, x=-200, y=-100)
+spel_4 = play.new_box (color= "red", width = 100, height= 100, x=200, y=-100)
+
+@play.when_mouse_clicked()
+#wanneer een van de vierkantjes wordt geklikt moet je naar een spelletje gaan
+
+play.start_program()
--- pong.py
+++ pong.py
@@ -8,6 +8,8 @@ batje_2.start_physics(obeys_gravity=False, can_move=False)
 bal.start_physics(obeys_gravity=False, x_speed=200)
 x_speed = 0
 y_speed = 0
+
+#bewegingen 
 @play.when_key_pressed("up")
 def batje1_omhoog():
     batje_1.y += 10
@@ -26,20 +28,23 @@ def batje2_omlaag():
 
 @play.repeat_forever
 def bal_tegen_randen():
+    global y_speed
     if bal.y > play.screen.top or bal.y < play.screen.bottom:
         y_speed *= -1
 
 @play.repeat_forever
 def bal_tegen_batjes():
+    global x_speed
     if bal.is_touching(batje_1) or bal.is_touching(batje_2):
         x_speed *= -1
 
+#score
 score_links = 0
 score_rechts = 0
 
 score_tekst = play.new_text(f"{score_links} - {score_rechts}", font_size=50, y=180)
 @play.repeat_forever
-def score_check():
+async def score_check():
     global score_links, score_rechts, x_speed, y_speed
     if bal.x > (batje_1.x + 120):
         score_links += 1
@@ -47,6 +52,7 @@ def score_check():
         bal.y = 0
         x_speed *= -1
         score_tekst.words = f"{score_links} - {score_rechts}"
+        await play.timer(seconds=2)
 
     if bal.x < (batje_2.x - 120):
         score_rechts += 1
@@ -54,6 +60,7 @@ def score_check():
         bal.y = 0
         x_speed *= -1
         score_tekst.words = f"{score_links} - {score_rechts}"
+        await play.timer(seconds=2)
 
 
 play.start_program()
