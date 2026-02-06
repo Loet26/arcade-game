e2e5e835f16615cb43e9eb75cf24ea9702750fc7
Loet26 <224544@lekenlinge.nl>
2026-02-06 09:52:32 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -7,7 +7,7 @@ spel_2 = play.new_box (color= "blue", width = 100, height= 100, x=200, y=100)
 spel_3 = play.new_box (color= "green", width = 100, height= 100, x=-200, y=-100)
 spel_4 = play.new_box (color= "red", width = 100, height= 100, x=200, y=-100)
 
-@play.when_mouse_clicked()
+#@play.when_mouse_clicked()
 #wanneer een van de vierkantjes wordt geklikt moet je naar een spelletje gaan
 
 play.start_program()
