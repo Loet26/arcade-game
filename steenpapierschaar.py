5c5bda1e3ebfac118b9b3410cc0b08eb6998c12d
Loet26 <224544@lekenlinge.nl>
2026-02-27 10:35:33 +0100

    Update steenpapierschaar.py

--- steenpapierschaar.py
+++ steenpapierschaar.py
@@ -1,66 +1,87 @@
 import play 
 import random
-keuze = None
-steen = play.new_image('steen.png', y = -100, x= -150)
-papier = play.new_image('papier.png', y= -100, x= 0)
-schaar = play.new_image('schaar.png', y=-100, x= 150)
 
-opties = steen, papier, schaar
+@play.repeat_forever
+def altijd():
+    def add_extension(objectnaam):
+        return objectnaam+'.png'
 
-keuze_tekst = play.new_text("Welke kies je?", font_size=50, y=180)
-aftel_tekst = play.new_text("Rock, paper, scissors, shoot!", font_size=100, y =100).hide()
+    keuze = None
+    keuzenaam = None
+    steen = play.new_image('steen.png', y = -100, x= -200, size=30)
+    papier = play.new_image('papier.png', y= -100, x= 0, size = 50)
+    schaar = play.new_image('schaar.png', y=-100, x= 170, size =50)
+    steen.show()
+    papier.show()
+    schaar.show()
 
-score_computer = 0
-score_speler = 0  
+    opties = [steen, papier, schaar]
+    opties_names = ['steen', 'papier', 'schaar']
+    keuze_tekst = play.new_text("Welke kies je?", font_size=50, y=180)
+    aftel_tekst = play.new_text("Rock, paper, scissors, shoot!", font_size=50, y =100)
+    aftel_tekst.hide()
 
-@play.when_mouse_clicked(steen) 
-def keuze1():
-    global keuze
-    keuze = steen 
-    keuze_tekst.hide()
-    aftel_tekst.show()
+    score_computer = 0
+    score_speler = 0  
 
-@play.when_mouse_clicked(papier)
-def keuze2():
-    global keuze
-    keuze = papier
-    keuze_tekst.hide()
-    aftel_tekst.show()
+    @steen.when_clicked
+    def geklikt():
+        global keuze, keuzenaam, steen, papier, schaar
+        steen = play.new_box(height = 200, width = 730, color='white', y=-100)
+        keuze = steen
+        keuzenaam = "steen"
+        keuze_tekst.hide()
+        aftel_tekst.show()
+        uitslag()
 
-@play.when_mouse_clicked(schaar)
-def keuze3():
-    global keuze
-    keuze = schaar 
-    keuze_tekst.hide()
-    aftel_tekst.show()
+    @papier.when_clicked
+    def geklikt():
+        global keuze, keuzenaam, steen, papier, schaar
+        papier = play.new_box(height = 200, width = 730, color='white', y=-100)
+        keuze = papier
+        keuzenaam = 'papier'
+        keuze_tekst.hide()
+        aftel_tekst.show()
+        uitslag()
 
+    @schaar.when_clicked
+    def geklikt():
+        global keuze, keuzenaam, steen, papier, schaar
+        schaar = play.new_box(height = 200, width = 730, color='white', y=-100)
+        keuze = schaar
+        keuzenaam = 'schaar'
+        keuze_tekst.hide()
+        aftel_tekst.show()
+        uitslag()
 
-def uitslag():
-    global score_computer, score_speler
-    computer_keuze = play.random(opties)
-    computer_keuze.x = -150
-    keuze.x=150
-    if computer_keuze == keuze: 
-        play.new_text("Niemand wint")
-    elif computer_keuze == steen and keuze == schaar:
-        play.new_text("Computer wint een punt")
-        score_computer += 1
-    elif computer_keuze == steen and keuze== papier: 
-        play.new_text("Jij wint een punt")
-        score_speler +=1
-    elif computer_keuze == papier and keuze == schaar:
-        play.new_text("Jij wint een punt")
-        score_speler += 1
-    elif computer_keuze == papier and keuze == steen: 
-        play.new_text("Computer wint een punt")
-        score_computer += 1
-    elif computer_keuze == schaar and keuze == papier: 
-        play.new_text("Computer wint een punt")
-        score_computer += 1
-    elif computer_keuze == schaar and keuze == steen: 
-        play.new_text("Jij wint een punt")
-        score_speler += 1
-        
-score_tekst = play.new_text(f"{score_computer} - {score_speler}", font_size=50, y=180)
+    def uitslag():
+        global keuze, keuzenaam
+        global score_computer, score_speler
+        if keuzenaam!= None:
+            computer_keuze = random.choice(opties_names)
+            computer_afbeelding = play.new_image(add_extension(computer_keuze), y = -100, x= 150, size=30)
+            speler_afbeelding = play.new_image(add_extension(keuzenaam), y = -100, x= -150, size=30)
+            if computer_keuze == keuzenaam: 
+                play.new_text("Niemand wint")
+            elif computer_keuze == "steen" and keuzenaam == 'schaar':
+                play.new_text("Computer wint een punt")
+                score_computer += 1
+            elif computer_keuze == 'steen' and keuzenaam== 'papier': 
+                play.new_text("Jij wint een punt")
+                score_speler +=1
+            elif computer_keuze == 'papier' and keuzenaam == 'schaar':
+                play.new_text("Jij wint een punt")
+                score_speler += 1
+            elif computer_keuze == 'papier' and keuzenaam == 'steen': 
+                play.new_text("Computer wint een punt")
+                score_computer += 1
+            elif computer_keuze == 'schaar' and keuzenaam == 'papier': 
+                play.new_text("Computer wint een punt")
+                score_computer += 1
+            elif computer_keuze == 'schaar' and keuzenaam == 'steen': 
+                play.new_text("Jij wint een punt")
+                score_speler += 1
+                
+        score_tekst = play.new_text(f"{score_computer} - {score_speler}", font_size=30, y=220)
 
 play.start_program()
